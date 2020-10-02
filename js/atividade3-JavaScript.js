0b82235fc30d409fd7e05a0cbf328e6a39835cbe
Leonardo <leo_narita@hotmail.com>
2020-10-01 22:10:10 -0300

    Atividade de Javascript (02/10/2020)

---
+++ js/atividade3-JavaScript.js
@@ -0,0 +1,77 @@
+function calcularPreco() {
+    var n1 = parseInt(window.document.getElementById('value').value)
+    var n2 = parseInt(window.document.getElementById('kilo').value)
+
+    window.document.getElementById('result').innerHTML = "Valor final: R$ " + (n1 * n2)
+    window.alert(`Valor final: R$ ${n1 * n2}`)
+}
+
+function limparCampos() {
+
+    // Limpa os conteúdos dos elementos
+    document.getElementById("name").value = "";
+    document.getElementById("masculino").checked = false;
+    document.getElementById("feminino").checked = false;
+    document.getElementById("height").value = "";
+    document.getElementById("resultWeight").textContent = "";
+    
+    // posiciona (joga o foco) no elemento inNome
+    document.getElementById("inNome").focus();
+}
+
+function calculateWeight() {
+    var name = document.getElementById("name").value
+    var height = document.getElementById("height").value
+    var weight
+
+    if(document.getElementById("masculino").checked) 
+        weight = 22 * height * height
+    else 
+        weight = 21 * height * height
+
+    document.getElementById("resultWeight").innerHTML = `${name}, seu peso ideal é ${weight}`
+}
+
+function calcularParquimetro() {
+    var money = parseFloat(document.getElementById("money").value)
+
+    if(money < 1) {
+        window.alert("Valor insuficiente")
+    }
+    else {
+
+        if(money < 1.75)
+            document.getElementById("resultMoney").innerHTML = `Tempo: 30 min \n <br> Troco: R$ ${money - 1}` 
+        else if(money < 3)
+            document.getElementById("resultMoney").innerHTML = `Tempo: 60 min \n <br> Troco: R$ ${money - 1.75}` 
+        else
+            document.getElementById("resultMoney").innerHTML = `Tempo: 120 min \n <br> Troco: R$ ${money - 3}` 
+    }
+
+}
+
+function calcularChimchilas() {
+    var numerC = Number(document.getElementById("numberChinchilas").value)
+    var numberY = Number(document.getElementById("numberYears").value)
+
+    document.getElementById("resultChinchilas").innerHTML = ""
+
+    if(numerC < 2 || isNaN(numerC) || numberY == 0 || isNaN(numberY)) {
+        window.alert("Valor inválido")
+        return;
+    }
+    else {
+        for(let i=0; i<numberY; i++) {
+            document.getElementById("resultChinchilas").innerHTML += `${i+1}º ano: ${numerC} Chinchilas\n <br>`
+            numerC *= 3
+        }
+    }
+}
+
+var btLimpar = document.getElementById("btClean");
+btLimpar.addEventListener("click", limparCampos);
+
+var btLimpar = document.getElementById("btnCalculate");
+btLimpar.addEventListener("click", calculateWeight);
+
+    
